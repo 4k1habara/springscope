ed6750e16c558c7d275035d08e5b9c6e2de4e3fa
shawt <shawtyhead@mail.ru>
2025-02-01 19:38:35 +0300

    basket realized

--- src/main/java/com/springhw/basket/Basket.java
+++ src/main/java/com/springhw/basket/Basket.java
@@ -1,4 +1,25 @@
 package com.springhw.basket;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+@Scope("prototype")
 public class Basket {
+    Set<Integer> basket = new HashSet<>();
+
+    public Set<Integer> getBasket() {
+        return basket;
+    }
+
+    public void setBasket(Set<Integer> basket) {
+        this.basket = basket;
+    }
+
+    public void addOrder(Set<Integer> order) {
+        basket.addAll(order);
+    }
 }
